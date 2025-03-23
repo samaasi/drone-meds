9effd5b2ef2e68ca98086febb54d8baef169a670
Benson Samaasi <dev.bensonsamaasi@gmail.com>
2025-03-23 10:42:17 +0100

    add endpoint to get available drone

--- cmd/routes/droneRoutes.ts
+++ cmd/routes/droneRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { CreateDroneSchema } from '../schemas/droneSchema';
 import { validateRequest } from "../middlewares/validateRequest";
-import { CreateDrone } from '../controllers/droneController';
+import {
+    CreateDrone,
+    GetAvailableDrones
+} from '../controllers/dispatchController';
 
 const router = Router();
 
 /** POST: Register a drone */
 router.post('/drones', validateRequest(CreateDroneSchema), CreateDrone);
+router.get("/drones/available", GetAvailableDrones);
 
 export default router;

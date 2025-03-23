a6ebd2e04d072729bedcae675b2b410612f2ccbd
Benson Samaasi <dev.bensonsamaasi@gmail.com>
2025-03-23 06:16:24 +0100

    refactor CreateDrone to use asyncHandler and ResponseUtility for error handling

--- cmd/controllers/droneController.ts
+++ cmd/controllers/droneController.ts
@@ -1,24 +1,22 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import ResponseUtility from '../utils/response';
+import { asyncHandler } from "../middlewares/errorHandler";
 
 const prisma = new PrismaClient();
 
-export const CreateDrone = async (req: Request, res: Response): Promise<void> => {
+export const CreateDrone = asyncHandler(async (req: Request, res: Response): Promise<void> => {
     const { model, weightLimit, batteryCapacity, state } = req.body;
 
-    try {
-        const drone = await prisma.drone.create({
-            data: {
-                serialNumber: `Drone-${Math.floor(Math.random() * 1000000)}`,
-                model: model,
-                weightLimit: weightLimit,
-                batteryCapacity: batteryCapacity,
-                state: state,
-            },
-        });
+    const drone = await prisma.drone.create({
+        data: {
+            serialNumber: `Drone-${Math.floor(Math.random() * 1000000)}`,
+            model: model,
+            weightLimit: weightLimit,
+            batteryCapacity: batteryCapacity,
+            state: state,
+        },
+    });
 
-        res.status(201).json(drone);
-    } catch (error) {
-        console.log(error);
-    }
-}
+    return ResponseUtility.created(res, drone);
+});

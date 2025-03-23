fb04c803cb22427918be4800e12ecf2d49ee6cae
Benson Samaasi <dev.bensonsamaasi@gmail.com>
2025-03-23 10:41:44 +0100

    Refactor droneController.ts to dispatchController.ts Implement Get available drones.

---
+++ cmd/controllers/dispatchController.ts
@@ -0,0 +1,46 @@
+import prisma from "../utils/prisma";
+import { Request, Response } from "express";
+import { DroneState } from "@prisma/client";
+import ResponseUtility from "../utils/response";
+import { asyncHandler } from "../middlewares/errorHandler";
+import { generateDroneSerialNumber } from "../utils/generateSerialNumber";
+
+/** registering a drone */
+export const CreateDrone = asyncHandler(async (req: Request, res: Response): Promise<void> => {
+    const { model, weightLimit, batteryCapacity, state } = req.body;
+
+    const drone = await prisma.drone.create({
+        data: {
+            serialNumber: generateDroneSerialNumber(),
+            model: model,
+            weightLimit: weightLimit,
+            batteryCapacity: batteryCapacity,
+            state: state,
+        },
+    });
+
+    return ResponseUtility.created(res, drone);
+});
+
+/** checking available drones for loading */
+export const GetAvailableDrones = asyncHandler(async (req: Request, res: Response): Promise<void> => {
+    const drones = await prisma.drone.findMany({
+        where: {
+            AND: [
+                { state: DroneState.IDLE },
+                { batteryCapacity: { gte: 25 } },
+            ],
+        },
+    });
+
+    return ResponseUtility.success(res, drones);
+});
+
+/** check drone battery level for a given drone */
+export const CheckDroneBatteryLevel = asyncHandler(async (req: Request, res: Response): Promise<void> => {
+    //
+});
+
+// loading a drone with medication items
+// checking loaded medication items for a given drone
+

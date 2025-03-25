8b3ed171d1647d53d634588187b64e3336771160
Benson Samaasi <dev.bensonsamaasi@gmail.com>
2025-03-25 11:18:11 +0100

    add medication routes for creation and retrieval

--- cmd/routes/droneRoutes.ts
+++ cmd/routes/droneRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import {
     CreateDrone,
+    CreateMedication,
+    GetAllMedications,
     GetAvailableDrones,
     CheckDroneBatteryLevel,
+    GetUnloadedMedications,
     LoadDroneWithMedication,
     CheckDroneLoadedMedication,
 } from '../controllers/dispatchController';
@@ -21,5 +24,9 @@ router.post(
 );
 router.get('/drones/:id/battery', CheckDroneBatteryLevel);
 router.get('/drones/:id/medications', CheckDroneLoadedMedication);
+
+router.post('/medications', CreateMedication);
+router.get('/medications', GetAllMedications);
+router.get('/medications/unloaded', GetUnloadedMedications);
 
 export default router;
